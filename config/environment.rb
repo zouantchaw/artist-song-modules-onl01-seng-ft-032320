97903e1d29776f63a53730c0a1d0e421168cba6f
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-09 05:15:02 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,9 @@ require 'pry'
 #require_relative '../lib/concerns/memorable'
 # require_relative '../lib/concerns/paramable'
 
-require_relative '../lib/concerns/findable'
+
 require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
