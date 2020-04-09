c383b10195116f7611b15fd5f21b226fafb307ba
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-09 05:37:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods 
+  include Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :artist
