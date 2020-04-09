3ef3c8a5be153fb3144c9bf9ebe559e6a336b0f0
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-09 05:40:10 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,20 @@
+module Memorable
+  
+  module ClassMethods 
+    
+    def reset_all
+    all.clear
+    end
+    
+    def count
+    all.count
+    end
+  end 
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+  
+end
